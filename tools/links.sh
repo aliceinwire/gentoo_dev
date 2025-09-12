531e7fe32e00f6b1a8d98a8fcbb3fc56a426481e
Arisu Tachibana <alicef@gentoo.org>
2025-09-12 12:25:48 +0900

    tools/links: Add tool for creating genpatches scripts symlink
    Signed-off-by: Arisu Tachibana <alicef@gentoo.org>

---
+++ tools/links.sh
@@ -0,0 +1,11 @@
+#!/bin/env bash
+
+ln -s /home/"${USER}"/gentoo_dev/repos/genpatches-misc/web/email-announcement.pl /usr/local/bin/
+ln -s /home/"${USER}"/gentoo_dev/repos/genpatches-misc/scripts/get-patch /usr/local/bin/
+ln -s /home/"${USER}"/gentoo_dev/repos/genpatches-misc/scripts/gpcreatebranch /usr/local/bin/
+ln -s /home/"${USER}"/gentoo_dev/repos/genpatches-misc/scripts/gpdoemail /usr/local/bin/
+ln -s /home/"${USER}"/gentoo_dev/repos/genpatches-misc/scripts/gpdorelease /usr/local/bin/
+ln -s /home/"${USER}"/gentoo_dev/repos/genpatches-misc/scripts/gpdoweb /usr/local/bin/
+ln -s /home/"${USER}"/gentoo_dev/repos/genpatches-misc/scripts/gpmultipatch /usr/local/bin/
+ln -s /home/"${USER}"/gentoo_dev/repos/genpatches-misc/scripts/gpmvhistorical /usr/local/bin/
+ln -s /home/"${USER}"/gentoo_dev/repos/genpatches-misc/scripts/gppatch /usr/local/bin/
